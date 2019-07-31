c158bff0e7d5fdb24da34c9163a2be345b73779a
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2019-07-31 12:02:28 +0530

    adding bull system

---
+++ cron_worker/bullSystem.js
@@ -0,0 +1,63 @@
+const Bull = require("bull");
+
+const env = process.env["NODE_ENV"];
+const removeOnComplete = true;
+let bullPrefix = "bull-default";
+
+if (env === "production") {
+    bullPrefix = "{bull-production}";
+} else if (env === "staging") {
+    bullPrefix = "{bull-staging}";
+} else if (env === "test") {
+    bullPrefix = "{bull-test}";
+} else {
+    bullPrefix = "{bull-development}";
+}
+
+const bullSystem = {};
+
+bullSystem.initBull = function() {
+    console.log(
+        "Bull redis",
+        `redis://${process.env["REDIS_HOST"]}:${
+            process.env["REDIS_PORT"]
+        } @ ${bullPrefix}`
+    );
+    const Queue = new Bull(
+        "dataQueue",
+        `redis://:${process.env["REDIS_PASS"]}@${process.env["REDIS_HOST"]}:${
+            process.env["REDIS_PORT"]
+        }`,
+        {
+            prefix: bullPrefix
+        }
+    );
+
+    bullSystem.bullDataJobs = Queue;
+
+    // Delay in ms
+    bullSystem.addJob = (
+        name,
+        data,
+        {
+            attempts = 3,
+            delay = 0,
+            timeout = 120000,
+            backOffDelay = 10000,
+            jobId
+        } = {}
+    ) => {
+        const jobOptions = {
+            attempts,
+            backoff: { type: "fixed", delay: backOffDelay },
+            delay,
+            timeout,
+            removeOnComplete,
+            jobId
+        };
+        return Queue.add(name, data, jobOptions);
+    };
+};
+bullSystem.initBull();
+
+module.exports = { bullSystem };
