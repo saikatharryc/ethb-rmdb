5fa9bca0db895c19b696637fda6199d9f4dcda18
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2019-07-31 15:38:33 +0530

    adding agenda for cronning

---
+++ cron_worker/agenda_processor.js
@@ -0,0 +1,26 @@
+const Agenda = require("agenda");
+const repetableJob = require("./cron");
+
+const agenda = new Agenda({
+    db: { address: process.env.MONGO_URI },
+    collection: "agendaJobs"
+});
+
+agenda.define(
+    "run repetableJob",
+    { priority: "high", concurrency: 1 },
+    async (job, done) => {
+        try {
+            await repetableJob(done);
+            await agenda.schedule("in 5 minutes", "run repetableJob", {});
+        } catch (ex) {
+            done(ex);
+        }
+    }
+);
+(async function() {
+    // IIFE to give access to async/await
+    await agenda.start();
+    await agenda.schedule("in 0 minutes", "run repetableJob", {});
+})();
+module.exports = agenda;
