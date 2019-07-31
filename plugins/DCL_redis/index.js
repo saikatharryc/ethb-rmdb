0515b0ce3c8d1a650171636ac2bb1599ce5d66e9
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2019-07-31 11:45:21 +0530

    slight changes

--- plugins/DCL_redis/index.js
+++ plugins/DCL_redis/index.js
@@ -3,10 +3,7 @@
 const fp = require('fastify-plugin')
 
 module.exports = fp(function(fastify, opts, next) {
-    // get redis client object
-    const {redis} = fastify
     //register the mailing service producer
     fastify.decorate('bullSystem', require('./bull'))
-
 next();
 })
--- plugins/support.js
+++ plugins/support.js
@@ -5,9 +5,9 @@ const fp = require('fastify-plugin')
 // the use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 
-module.exports = fp(function (fastify, opts, next) {
-  fastify.decorate('someSupport', function () {
-    return 'hugs'
+module.exports = fp( (fastify, opts, next) =>{
+  fastify.decorate('getTransactions',  (userAddress)=> {
+    await fastify.mongo.models.transactions.find({})
   })
   next()
 })
